58d40ad94c7745b858304f4d27b51862286a3fed
benmacdonald <ben@sportstats.ca>
2016-11-17 14:53:18 -0500

    Initial commit

---
+++ app/src/main/java/com/uottawa/benjaminmacdonald/cooking_app/Adapters/FavouriteArrayAdapter.java
@@ -0,0 +1,37 @@
+package com.uottawa.benjaminmacdonald.cooking_app.Adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import com.uottawa.benjaminmacdonald.cooking_app.R;
+
+import java.util.List;
+
+/**
+ * Created by BenjaminMacDonald on 2016-11-11.
+ */
+
+public class FavouriteArrayAdapter extends ArrayAdapter<String> { //CHANGE TO RECIPE
+    private final Context context;
+    private final List<String> values;
+
+    public FavouriteArrayAdapter(Context context, List<String> values) {
+        super(context, R.layout.recipe_favourite_item_layout,values);
+        this.values = values;
+        this.context = context;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View cardView = inflater.inflate(R.layout.recipe_favourite_item_layout,parent,false);
+        TextView nameView = (TextView) cardView.findViewById(R.id.favTextView);
+        nameView.setText(values.get(position));
+
+        return cardView;
+    }
+}
---
+++ app/src/main/java/com/uottawa/benjaminmacdonald/cooking_app/MainActivity.java
@@ -0,0 +1,115 @@
+package com.uottawa.benjaminmacdonald.cooking_app;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.uottawa.benjaminmacdonald.cooking_app.Adapters.FavouriteArrayAdapter;
+import com.uottawa.benjaminmacdonald.cooking_app.Adapters.RecipeArrayAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    List<String> favourites = new ArrayList<String>();
+    List<String> recipes = new ArrayList<String>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        //*************************Setting up favourite view ***************************************
+
+        GridView gridView = (GridView) findViewById(R.id.gridView);
+        favourites = new ArrayList<String>();
+
+        for (int i = 0; i<10; i++){
+            favourites.add("Test"+i);
+            recipes.add("Test "+i);
+        }
+
+        FavouriteArrayAdapter favArrayAdapter = new FavouriteArrayAdapter(this,favourites);
+        gridView.setAdapter(favArrayAdapter);
+        gridView.setNumColumns(favourites.size());
+        setDynamicWidth(gridView);
+        gridView.setDrawSelectorOnTop(true);
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast toast = Toast.makeText(getApplication().getBaseContext(),
+                        String.valueOf(position), Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        });
+
+        //*************************Setting up recipe list view ***************************************
+        ListView listView = (ListView) findViewById(R.id.recipeListView);
+
+        RecipeArrayAdapter recipeArrayAdapter = new RecipeArrayAdapter(this,recipes);
+        listView.setAdapter(recipeArrayAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    //FROM STACKOVERFLOW http://stackoverflow.com/questions/5725745/horizontal-scrolling-grid-view
+
+    private void setDynamicWidth(GridView gridView) {
+        ListAdapter gridViewAdapter = gridView.getAdapter();
+        if (gridViewAdapter == null) {
+            return;
+        }
+        int totalWidth;
+        int items = gridViewAdapter.getCount();
+        View listItem = gridViewAdapter.getView(0, null, gridView);
+        listItem.measure(0, 0);
+        totalWidth = listItem.getMeasuredWidth();
+        totalWidth = totalWidth*items;
+        ViewGroup.LayoutParams params = gridView.getLayoutParams();
+        params.width = totalWidth;
+        gridView.setLayoutParams(params);
+    }
+
+}
