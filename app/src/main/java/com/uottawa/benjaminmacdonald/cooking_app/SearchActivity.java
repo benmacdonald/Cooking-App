2d778473c288de526877213408a1f1173f0b9848
Tommy Nguyen <tommy.nguyen7@hotmail.com>
2016-11-22 17:08:01 -0500

    Finished search activity

--- app/src/main/java/com/uottawa/benjaminmacdonald/cooking_app/SearchActivity.java
+++ app/src/main/java/com/uottawa/benjaminmacdonald/cooking_app/SearchActivity.java
@@ -1,10 +1,14 @@
 package com.uottawa.benjaminmacdonald.cooking_app;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Spinner;
+
+import com.uottawa.benjaminmacdonald.cooking_app.Adapters.RecipeArrayAdapter;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,6 +21,7 @@ public class SearchActivity extends AppCompatActivity {
     List<String> typeArray;
     List<String> cultureArray;
     List<String> healthyArray;
+    List<String> recipes = new ArrayList<String>();
 
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
@@ -28,30 +33,30 @@ public class SearchActivity extends AppCompatActivity {
 
         //******************* SETTING UP FLITERS *******************************
         typeArray = new ArrayList<String>();
-        typeArray.add("BREAKFAST");
-        typeArray.add("LUNCH");
-        typeArray.add("DINNER");
-        typeArray.add("DESERT");
-        typeArray.add("DRINK");
+        typeArray.add("Breakfast");
+        typeArray.add("Lunch");
+        typeArray.add("Dinner");
+        typeArray.add("Desert");
+        typeArray.add("Drink");
 
         cultureArray = new ArrayList<String>();
-        cultureArray.add("CHINESE");
-        cultureArray.add("INDIAN");
-        cultureArray.add("ITALIAN");
-        cultureArray.add("AMERICAN");
+        cultureArray.add("Chinese");
+        cultureArray.add("Indian");
+        cultureArray.add("Italian");
+        cultureArray.add("American");
 
         healthyArray = new ArrayList<String>();
-        healthyArray.add("YES");
-        healthyArray.add("NO");
+        healthyArray.add("Healthy");
+        healthyArray.add("Non-healthy");
 
-        ArrayAdapter<String> filterTypeAdapter = new ArrayAdapter<String>(this,R.layout.support_simple_spinner_dropdown_item,typeArray);
-        filterTypeAdapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+        ArrayAdapter<String> filterTypeAdapter = new ArrayAdapter<String>(this,R.layout.spinner_item,typeArray);
+        filterTypeAdapter.setDropDownViewResource(R.layout.spinner_item_expanded);
 
-        ArrayAdapter<String> filterCultureAdapter = new ArrayAdapter<String>(this,R.layout.support_simple_spinner_dropdown_item,cultureArray);
-        filterCultureAdapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+        ArrayAdapter<String> filterCultureAdapter = new ArrayAdapter<String>(this,R.layout.spinner_item,cultureArray);
+        filterCultureAdapter.setDropDownViewResource(R.layout.spinner_item_expanded);
 
-        ArrayAdapter<String> filterHealthyAdapter = new ArrayAdapter<String>(this,R.layout.support_simple_spinner_dropdown_item,healthyArray);
-        filterHealthyAdapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+        ArrayAdapter<String> filterHealthyAdapter = new ArrayAdapter<String>(this,R.layout.spinner_item,healthyArray);
+        filterHealthyAdapter.setDropDownViewResource(R.layout.spinner_item_expanded);
 
         Spinner type = (Spinner) findViewById(R.id.typeSpinner);
         type.setAdapter(filterTypeAdapter);
@@ -61,5 +66,17 @@ public class SearchActivity extends AppCompatActivity {
 
         Spinner healthy = (Spinner) findViewById(R.id.healthySpinner);
         healthy.setAdapter(filterHealthyAdapter);
+
+        //*************************Setting up recipe list view ***************************************
+
+        //for testing purposes
+        for (int i = 0; i<10; i++){
+            recipes.add("Test "+i);
+        }
+
+        ListView listView = (ListView) findViewById(R.id.recipeListView);
+
+        RecipeArrayAdapter recipeArrayAdapter = new RecipeArrayAdapter(this,recipes);
+        listView.setAdapter(recipeArrayAdapter);
     }
 }
