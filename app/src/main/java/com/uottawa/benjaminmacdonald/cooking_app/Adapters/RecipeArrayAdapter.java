4ae8eda3e4ce803924e1e54872f18454ef754384
benmacdonald <ben@sportstats.ca>
2016-12-04 00:30:25 -0500

    fixed the crashing when saving a recipe a returning to the list fast

--- app/src/main/java/com/uottawa/benjaminmacdonald/cooking_app/Adapters/RecipeArrayAdapter.java
+++ app/src/main/java/com/uottawa/benjaminmacdonald/cooking_app/Adapters/RecipeArrayAdapter.java
@@ -71,14 +71,16 @@ public class RecipeArrayAdapter extends ArrayAdapter<Recipe> {
         protected Bitmap doInBackground(String... params) {
             RealmUtils futureRealmUtils = new RealmUtils(context);
             Bitmap bitmap = bitmapCache.get(params[0]);
-            if(bitmapCache.get(params[0]) != null){
-                futureRealmUtils.getRealm().close();
-                return bitmapCache.get(params[0]);
-            } else {
-                bitmapCache.put(params[0],futureRealmUtils.convertToBitmap(bytes));
-                futureRealmUtils.getRealm().close();
-                return futureRealmUtils.convertToBitmap(bytes);
+            if(bitmap == null){
+                while(bytes == null){
+                    futureRealmUtils.getRealm().waitForChange();
+                    bytes = futureRealmUtils.getRecipeFromID(params[0]).getPhoto();
+                }
+                bitmap = futureRealmUtils.convertToBitmap(bytes);
+                bitmapCache.put(params[0],bitmap);
             }
+//            futureRealmUtils.getRealm().close();
+            return bitmap;
 
         }
 
